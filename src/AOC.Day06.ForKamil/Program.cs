94451438a57ec0a70f55e3113af11a4fb603f42f
LukaszAdamus <lukasz.adamus@soodo.pl>
2020-12-06 22:22:06 +0100

    ...

--- src/AOC.Day06.ForKamil/Program.cs
+++ src/AOC.Day06.ForKamil/Program.cs
@@ -2,17 +2,19 @@
 using System.IO;
 using System.Linq;
 
+var nl = Environment.NewLine;
+
 var input = File
     .ReadAllText("input.txt")
-    .Split($"{Environment.NewLine}{Environment.NewLine}");
+    .Split($"{nl}{nl}");
 
 var a = input
-    .Sum(x => x.Replace(Environment.NewLine, "")
+    .Sum(x => x.Replace(nl, "")
         .Distinct()
         .Count());
 
 var b = input
-    .Sum(x => x.Split(Environment.NewLine)
+    .Sum(x => x.Split(nl)
         .Aggregate((a, b) => string.Concat(a.Intersect(b)))
         .Distinct()
         .Count());
