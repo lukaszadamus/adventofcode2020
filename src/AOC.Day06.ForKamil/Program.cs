cc32d29212020204551088e2e40e7460e537b547
LukaszAdamus <lukasz.adamus@soodo.pl>
2020-12-06 22:16:54 +0100

    CR on phone

--- src/AOC.Day06.ForKamil/Program.cs
+++ src/AOC.Day06.ForKamil/Program.cs
@@ -4,6 +4,17 @@ using System.Linq;
 
 var input = File.ReadAllText("input.txt").Split($"{Environment.NewLine}{Environment.NewLine}");
 
-Console.WriteLine($"A:{input.Sum(x => x.Replace(Environment.NewLine, "").Distinct().Count())}");
-Console.WriteLine($"A:{input.Sum(x => x.Split(Environment.NewLine).Aggregate((a, b) => string.Concat(a.Intersect(b))).Distinct().Count())}");
+var a = input
+    .Sum(x => x.Replace(Environment.NewLine, "")
+        .Distinct()
+        .Count());
+
+var b = input
+    .Sum(x => x.Split(Environment.NewLine)
+        .Aggregate((a, b) => string.Concat(a.Intersect(b)))
+        .Distinct()
+        .Count());
+
+Console.WriteLine($"A:{a}");
+Console.WriteLine($"B:{b}");
 
