e55b83a5e3128c6e01b89e2dc5be143faf2e1ec8
LukaszAdamus <lukasz.adamus@soodo.pl>
2020-12-06 22:06:49 +0100

    Day 06 for Kamil ;)

---
+++ src/AOC.Day06.ForKamil/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var input = File.ReadAllText("input.txt").Split($"{Environment.NewLine}{Environment.NewLine}");
+
+Console.WriteLine($"A:{input.Sum(x => x.Replace(Environment.NewLine, "").Distinct().Count())}");
+Console.WriteLine($"A:{input.Sum(x => x.Split(Environment.NewLine).Aggregate((a, b) => string.Concat(a.Intersect(b))).Distinct().Count())}");
+
