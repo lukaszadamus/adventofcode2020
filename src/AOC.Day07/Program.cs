a813fb37d94b98e655458912020de8af39034b5f
LukaszAdamus <lukasz.adamus@soodo.pl>
2020-12-08 00:40:00 +0100

    Day 07

---
+++ src/AOC.Day07/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AOC.Shared;
+
+Configure.Logging();
+Dictionary<string, Content[]> input;
+
+using (var _ = new DiagnosticHelper("Read data"))
+{
+    input = File.ReadAllLines("input.txt").GetRules();
+}
+
+var a = SolveA(input, "shiny gold");
+var b = SolveB(input, "shiny gold");
+
+Console.WriteLine($"A:{a}");
+Console.WriteLine($"B:{b}");
+
+int SolveA(Dictionary<string, Content[]> input, string colorToCheck)
+{
+    using var _ = new DiagnosticHelper("Day07.A");
+    var count = 0;
+
+    foreach (var rule in input)
+    {
+        var queue = new Queue<string>();
+        queue.Enqueue(rule.Key);
+
+        var found = false;
+        while (queue.Count > 0)
+        {
+            found = false;
+
+            var current = queue.Dequeue();
+
+            var colors = input[current].Select(x => x.Color);
+
+            if (colors.Any(x => x == colorToCheck))
+            {
+                found = true;
+                queue.Clear();
+            }
+            else
+            {
+                foreach (var next in colors.Where(x => x != "no other"))
+                {
+                    queue.Enqueue(next);
+                }
+            }
+        }
+        count += found ? 1 : 0;
+    }
+
+    return count;
+}
+
+int SolveB(Dictionary<string, Content[]> input, string colorToCheck)
+{
+    using var _ = new DiagnosticHelper("Day07.B");
+    var count = 0;
+
+    var queue = new Queue<(string, int)>();
+    queue.Enqueue((colorToCheck, 1));
+
+    while (queue.Count > 0)
+    {
+        var (currentColor, f) = queue.Dequeue();
+
+        if (currentColor == "no other")
+        {
+            count += f * 1;
+        }
+        else
+        {
+            var content = input[currentColor];
+            var localCount = f * content.Sum(x => x.Quantity);
+            count += localCount;
+
+            foreach (var next in content)
+            {
+                queue.Enqueue((next.Color, f * next.Quantity));
+            }
+        }
+    }
+
+    return count;
+}
---
+++ src/AOC.Day07/Utils.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public record Content
+{
+    private static readonly Regex _rx = new Regex(@"^(?<q>\d+)([\s])(?<c>[\w]+\s[\w]+)$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+    public int Quantity { get; }
+    public string Color { get; }
+
+    public Content(string toParse)
+    {
+        if (!toParse.Equals("no other", StringComparison.InvariantCultureIgnoreCase))
+        {
+            var m = _rx.Match(toParse);
+
+            Quantity = int.Parse(m.Groups["q"].Value);
+            Color = m.Groups["c"].Value;
+        }
+        else
+        {
+            Color = "no other";
+            Quantity = 0;
+        }
+    }
+}
+
+public static class Extensions
+{
+    public static string Clean(this string value)
+        => value
+            .Replace(" bags", "")
+            .Replace(" bag", "")
+            .Replace(".", "")
+            .Replace(", ", ",")
+            .Trim();
+
+    public static Dictionary<string, Content[]> GetRules(this string[] lines)
+        => lines.Select(x =>
+        {
+            var rl = x.Clean().Split(" contain ");
+            var key = rl[0];
+            var value = rl[1].Split(",").Select(x => new Content(x)).ToArray();
+            return new { k = key, v = value };
+        }).ToDictionary(x => x.k, x => x.v);
+}
