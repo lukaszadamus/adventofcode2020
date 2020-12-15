1bc6f05c4e5148f79d78a2acbe4d4887d09d8a56
LukaszAdamus <lukasz.adamus@soodo.pl>
2020-12-15 13:02:42 +0100

    :lipstick:

--- src/AOC.Day15/Program.cs
+++ src/AOC.Day15/Program.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using AOC.Shared;
+
+Configure.Logging();
 
 var numbers = File.ReadAllText("input.txt")
     .Split(",")
@@ -12,10 +15,11 @@ var a = SolveA(numbers, 2020);
 Console.WriteLine($"A: {a}");
 
 var b = SolveB(numbers, 30000000);
-Console.WriteLine($"A: {b}");
+Console.WriteLine($"B: {b}");
 
 int SolveA(int[] numbers, int round)
 {
+    using var _ = new DiagnosticHelper("SolveA");
     var numberRounds = numbers
         .Select((x, i) => new { n = x, i = i + 1 })
         .ToDictionary(x => x.n, x => new List<int> { x.i });
@@ -50,6 +54,7 @@ int SolveA(int[] numbers, int round)
 
 int SolveB(int[] numbers, int round)
 {
+    using var _ = new DiagnosticHelper("SolveB");
     var numberRounds = numbers
         .Select((x, i) => new { n = x, i = i + 1 })
         .ToDictionary(x => x.n, x => new int[2] { x.i, x.i });
